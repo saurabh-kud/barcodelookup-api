401a4bd24936601a894167c08400d0fcdafd66ff
saurabh-kud <saurabh322001raj@gmail.com>
2024-04-02 00:57:10 +0530

    v.1.0.1

---
+++ getBarcodeData.js
@@ -0,0 +1,58 @@
+const puppeteer = require("puppeteer");
+
+const getBarCode = async (barCodeId) => {
+  let browser;
+  try {
+    // Launch the browser and open a new blank page
+    browser = await puppeteer.launch({
+      headless: false,
+      defaultViewport: null, // otherwise it defaults to 800x600
+      args: ["--no-sandbox", "--start-fullscreen"],
+    });
+    const page = await browser.newPage();
+
+    // Navigate the page to a URL
+    await page.goto("https://www.barcodelookup.com/" + barCodeId, {
+      waitUntil: "load",
+    });
+
+    // Get the image URL
+    const imageUrl = await page.evaluate(() => {
+      const imageElement = document.querySelector("#largeProductImage img");
+      return imageElement.getAttribute("src");
+    });
+    // console.log("Image URL:", imageUrl);
+
+    // Get the title
+
+    const title = await page.evaluate(() => {
+      const titleElement = document.querySelector(
+        ".container .row .product-details h4"
+      );
+      return titleElement.textContent.trim();
+    });
+
+    const categoty = await page.evaluate(() => {
+      const titleElements = document.querySelectorAll(
+        ".container .row .product-details div.product-text-label"
+      );
+      const lastTitleElement = titleElements[1];
+      // Selecting the second element
+      const categotyfinal = lastTitleElement.textContent.substring(10);
+      return categotyfinal;
+    });
+
+    await browser.close();
+    return { title, imageUrl, categoty };
+  } catch (error) {
+    await browser.close();
+    return { error };
+  } finally {
+    // Close the browser
+    if (browser) {
+      await browser.close();
+    }
+  }
+};
+
+module.exports = getBarCode;
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const getBarCode = require("./getBarcodeData");
+const app = express();
+
+//get data from req body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/barcode/:id", async (req, res) => {
+  const barCodeId = req.params.id;
+  const data = await getBarCode(barCodeId);
+  if (!data?.categoty) {
+    res.json({
+      message: "successfully fetched the data",
+      data: {},
+      error: "barcode number is not valid",
+    });
+  } else {
+    res.json({ message: "successfully fetched the data", data, error: {} });
+  }
+});
+
+app.get("*", (req, res) => res.json({ message: "you are on home route" }));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("server is running on port number", PORT);
+});
